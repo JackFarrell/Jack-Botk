4d34702317b022556487d7f99fb46607ac04bf14
jackfarrell11 <jack.conner.farrell@gmail.com>
2021-05-27 09:14:58 -0400

    lihbg

---
+++ script.js
@@ -0,0 +1,10 @@
+var myName = "The J-Bot";
+var yourName = prompt("What is your name?");
+alert("Hi, " + yourName + "! My name is, " + myName + "!");
+var myThingToDo = "talking to you";
+var yourThingToDo = prompt("What's your favorite thing to do?");
+if (myThingToDo === yourThingToDo) {
+    alert("Wow! I like to do that too!")
+} else {
+    alert("Wow, you " + yourThingToDo + "! That sounds fun!" + " My favorite thing to do is " + myThingToDo + " Probably because I don't have legs, arms, physical brain, emotions, thumbs, awesome toes like Jack, my creator, a sense of humor, and so on...");
+};
